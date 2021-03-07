32e378f9f0fb8e277f131f70fc7001dcd71ad76f
keien411 <1720659422@qq.com>
2021-03-07 13:43:32 +0800

    第一版提交

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,21 +18,174 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import java.util.Date
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                HelloInput()
             }
         }
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun HelloInput(
+
+) {
+    Column {
+        var isStart by remember { mutableStateOf(true) }
+
+        val textState = remember { mutableStateOf(TextFieldValue(text = "8")) }
+
+        val defaultModifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+
+        OutlinedTextField(
+            value = textState.value,
+            onValueChange = {
+                textState.value = it
+            },
+            modifier = defaultModifier,
+            textStyle = MaterialTheme.typography.h6,
+            enabled = isStart,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+        Button(onClick = {
+            val task = MyTimerTask(textState.value.text.toInt(), textState, startFun = {
+                isStart = it
+            })
+
+            Timer().schedule(task, Date(), 1000)
+            isStart = false
+        }, modifier = defaultModifier, enabled = isStart) {
+            Text(text = "开始")
+        }
+
+        AnimatedVisibility(
+            visible = !isStart,
+            enter = slideInVertically(initialOffsetY = { -40 }) + expandVertically(expandFrom = Alignment.Top) + fadeIn(
+                initialAlpha = 0.3f
+            ),
+            exit = slideOutVertically() + shrinkVertically() + fadeOut()
+
+        ) {
+            Text(modifier = defaultModifier, text = "开始倒计时啦")
+        }
+
+        val infiniteTransition = rememberInfiniteTransition()
+        val color by infiniteTransition.animateColor(
+            initialValue = Color.Red,
+            targetValue = Color.Green,
+            animationSpec = infiniteRepeatable(
+                animation = tween(1000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        val infiniteTransition2 = rememberInfiniteTransition()
+        val color2 by infiniteTransition2.animateColor(
+            initialValue = Color.Yellow,
+            targetValue = Color.Gray,
+            animationSpec = infiniteRepeatable(
+                animation = tween(1000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxHeight()
+                .background(color),
+            contentAlignment = Alignment.Center,
+        ) {
+
+            Text(
+                color = color2,
+                text = textState.value.text,
+                style = TextStyle(
+                    fontSize = 70.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+
+            )
+
+        }
+
+    }
+}
+
+class MyTimerTask(
+    private var time: Int,
+    private var textState: MutableState<TextFieldValue>,
+    private var startFun: (b: Boolean) -> Unit
+) :
+    TimerTask() {
+    override fun run() {
+        if (time == 0) {
+            this.cancel()
+            startFun(true)
+            return
+        }
+
+        time--
+        textState.value = TextFieldValue(text = time.toString())
+        println(time)
     }
 }
 
@@ -44,18 +197,20 @@ fun MyApp() {
     }
 }
 
+@ExperimentalAnimationApi
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        HelloInput()
     }
 }
 
+@ExperimentalAnimationApi
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        HelloInput()
     }
 }
